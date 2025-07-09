86de12b6182fa3927ddc1a2811c0022c911bbd72
Zhongfang Zhuang <zhongfang@ensemblecore.ai>
2025-07-09 09:30:08 -0700

    Update 03_install_pkgs.sh

--- 03_install_pkgs.sh
+++ 03_install_pkgs.sh
@@ -12,7 +12,7 @@ which python
 which pip
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
-pip install transformers diffusers tqdm timm wandb accelerate ninja packaging tensorboard easydict scikit-learn opencv-python datasets fvcore ptflops s3fs webdataset glances[gpu] matplotlib
+pip install tiktoken ipykernel transformers diffusers tqdm timm wandb accelerate ninja packaging tensorboard easydict scikit-learn opencv-python datasets fvcore ptflops s3fs webdataset glances[gpu] matplotlib
 pip install onnx onnxsim onnxruntime
 
 if [ "$INSTALL_FLASH_ATTN" = "y" ]; then
