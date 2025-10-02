51816e3e1c471c5350c4c394879614683824f92f
Zhongfang Zhuang <zhongfang@ensemblecore.ai>
2025-10-02 11:47:24 -0700

    Update 03_install_pkgs.sh

--- 03_install_pkgs.sh
+++ 03_install_pkgs.sh
@@ -11,13 +11,14 @@ fi
 which python
 which pip
 
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu129
-pip install unsloth tiktoken ipykernel transformers diffusers tqdm timm wandb accelerate ninja packaging tensorboard easydict scikit-learn opencv-python datasets fvcore ptflops s3fs webdataset glances[gpu] matplotlib onnx onnxsim onnxruntime llms_from_scratch tokenizers
-
-
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 if [ "$INSTALL_FLASH_ATTN" = "y" ]; then
     MAX_JOBS=16 pip install flash-attn --no-build-isolation
 fi
+pip install tf-keras
+pip install unsloth
+pip install thop tiktoken ipykernel transformers diffusers tqdm timm wandb accelerate ninja packaging tensorboard easydict scikit-learn opencv-python datasets fvcore ptflops s3fs webdataset glances[gpu] matplotlib onnx onnxsim onnxruntime llms_from_scratch tokenizers
+
 
 echo "All Python packages installed."
 
